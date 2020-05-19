cea31ea056fdfb7d21a34cf782db51de6b656783
audrey.ly <audrey.ly@u-psud.fr>
2020-05-19 19:20:45 +0200

    données nécessaires au lancement du quizz

---
+++ lancement.php
@@ -0,0 +1,20 @@
+<?php
+	include 'header.php';
+	var_dump($_POST);
+?>
+
+<div>
+  <form action="question.php" method="post" id="autoclick">
+    <div class="form-group">
+    	<input name="numquestion" id="numquestion" value="1"/>
+    	<input name="idQuizz" id="idQuizz" value="1"/>
+    	<input name="nomQuizz" id="nomQuizz" value="Disney"/>
+    	<input name="nomcat" id="nomcat" value="Films"/>
+    	<button type="submit" class="btn btn-primary">Valider</button>
+    </div>
+   </form>
+ </div>
+
+<script type="text/javascript">
+  	//document.getElementById("autoclick").submit();
+</script>
